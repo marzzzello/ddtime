c4e644e13d84e61ffb303282067e6040f20ed413
marzzzello <15198431+marzzzello@users.noreply.github.com>
2018-12-19 23:41:50 +0100

    some error handling and more variables

--- ddtime.sh
+++ ddtime.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+dir="`pwd`"
+dbname="ddnet.sqlite"
+db="$dir/$dbname"
+ddtime="ddtime.sh"
+
+if [ ! -f "$db" ]; then
+    echo "$dbname not found!";
+    echo "Make sure that you have downloaded the database and put it in the same folder as $ddtime ($dir)";
+    exit;
+fi 
+
 map="$1";
 
 name="$2";
@@ -12,16 +23,15 @@ if [[ -z $3 ]]; then
 fi
 
 if [[ -z $1 ]]; then
-	echo "Usage: ./time.sh map [name (default: $name)] [difftime in minutes (default: $t)]";
+	echo "Usage: ./$ddtime map [name (default: $name)] [difftime in minutes (default: $t)]";
 	exit;
 fi
 
-dir="`pwd`"
  
 echo "select Name, 
 (SELECT round((strftime('%s', Timestamp) - strftime('%s',(select timestamp from race where name='$name' and map='$map') )) / 60.0,1)) as 'Difference in minutes',
 round(Time/60) as 'Finish time in minutes' from race where Map='$map' 
 AND Timestamp >= (select datetime(timestamp, '-$t minutes') from race where name='$name' and map='$map') 
 AND Timestamp <= (select datetime(timestamp, '+$t minutes') from race where name='$name' and map='$map') 
-order by Timestamp asc;" | sqlite3 "$dir/ddnet.sqlite" --column --header
+order by Timestamp asc;" | sqlite3 "$db" --column --header
  
