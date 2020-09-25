091c0b2c0971545ca8af78aa166e86df825b249e
marzzzello <15198431+marzzzello@users.noreply.github.com>
2020-09-25 02:32:22 +0200

    autodownload db + fix working dir

--- ddtime.sh
+++ ddtime.sh
@@ -1,14 +1,21 @@
-#!/bin/bash
-dir="`pwd`"
-dbname="ddnet.sqlite"
-db="$dir/$dbname"
+#!/usr/bin/env bash
+dir="${BASH_SOURCE%/*}"
+cd "$dir"
+db="ddnet.sqlite"
 ddtime="ddtime.sh"
 
 if [ ! -f "$db" ]; then
-    echo "$dbname not found!";
+    echo "$db not found!";
     echo "Make sure that you have downloaded the database and put it in the same folder as $ddtime ($dir)";
-    exit;
-fi 
+	while true; do
+	    read -p "Do you wish to download $db from ddnet.tw? [y/n]" yn
+	    case $yn in
+	        [Yy]* ) wget "https://ddnet.tw/stats/ddnet.sqlite.zip" -q --show-progress; unzip "$db.zip" && rm "$db.zip"; break;;
+	        [Nn]* ) exit;;
+	        * ) echo "Please answer yes or no.";;
+	    esac
+	done
+fi
 
 map="$1";
 
@@ -17,7 +24,7 @@ if [[ -z $2 ]]; then
 	name="marzzzello"; #Change marzzzello here to your preferred default nickname
 fi
 
-t="$3"; 
+t="$3";
 if [[ -z $3 ]]; then
 	t="10"; 		#The default is to look the 10 minutes before and after your finish
 fi
@@ -31,14 +38,14 @@ IFS=$'\n'
 for OUTPUT in $(echo "select timestamp from race where name='$name' and map='$map'
 order by Timestamp asc;" | sqlite3 "$db" --noheader)
 do
-    
+
     echo $OUTPUT
-    echo "select Name, 
+    echo "select Name,
     round((strftime('%s', Timestamp) - strftime('%s', '$OUTPUT')) / 60.0,1) as 'Difference in minutes',
-    round(Time/60) as 'Finish time in minutes' 
-    from race where Map='$map' 
+    round(Time/60) as 'Finish time in minutes'
+    from race where Map='$map'
     AND Timestamp >= datetime('$OUTPUT', '-$t minutes')
     AND Timestamp <= datetime('$OUTPUT', '+$t minutes')
     order by Timestamp asc;" | sqlite3 "$db" --column --header
- 
+
 done
